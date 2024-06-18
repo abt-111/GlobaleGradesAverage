0aacfd1a186fe855ad232e98588822eb0f89c919
abt-111 <anthony.temmel@gmail.com>
2024-06-18 12:36:30 +0200

    Ajout des classe Campus et Student

---
+++ GlobaleGradesAverage/Campus.cs
@@ -0,0 +1,35 @@
+﻿namespace GlobaleGradesAverage
+{
+    internal class Campus
+    {
+        public Student[] students;
+
+        public void DisplayStudents()
+        {
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+
+        public float ComputeGradePointAverage(Student student)
+        {
+            float gradePointAverage = student.Grades.Sum() / student.Grades.Length;
+            return gradePointAverage;
+        }
+
+        public float ComputeGlobaleGradePointAverage()
+        {
+            float globaleGradePointAverage = 0.0f;
+
+            foreach (Student student in students)
+            {
+                globaleGradePointAverage += ComputeGradePointAverage(student);
+            }
+
+            globaleGradePointAverage /= students.Length;
+
+            return globaleGradePointAverage;
+        }
+    }
+}
---
+++ GlobaleGradesAverage/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace GlobaleGradesAverage
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public float[] Grades { get; set; }
+        public void SpeakWith(Student student)
+        {
+            Console.WriteLine($"Hi {student.Name}. How are you today?");
+        }
+
+        public void YellAt(Student student)
+        {
+            Console.WriteLine($"{student.Name}! Come here!");
+        }
+    }
+}
